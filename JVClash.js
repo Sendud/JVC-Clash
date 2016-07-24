1d83fe7d603c7199e9fb771a1b5e4bd4d05fa6b8
Sendud <sandysweet1992@gmail.com>
2016-07-24 01:21:28 +0100

    Update JVClash.js

--- JVClash.js
+++ JVClash.js
@@ -1,4 +1,25 @@
-
+// ==UserScript==
+// @name        JVCSticker++
+// @namespace   JVCSticker++
+// @include     http://www.jeuxvideo.com/forums/*
+// @include     https://www.jeuxvideo.com/forums/*
+// @version     1.5.20
+// @grant       GM_addStyle
+// @updateURL   https://ticki84.github.io/JVCSticker++.meta.js
+// @downloadURL https://ticki84.github.io/JVCSticker++.user.js
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require     https://ticki84.github.io/jquery.modal.min.js
+// @connect     github.io
+// @grant       GM_xmlhttpRequest
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_listValues
+// @grant       GM_deleteValue
+// @icon        http://jv.stkr.fr/p/1kki
+// @author      Ticki84
+// @copyright   2016+, Ticki84
+// @noframes
+// ==/UserScript==
 
 var mots=["Astalavista", "Cordialement", "Mes salutations", "toboggan", "dinosaurésque", "coupin", "gentille personne","truculent","être formidable"];
 var insultes=["va te faire enculer", "nique ta mère", "suce ma bite", "ta mère", "fils de pute", "fils", "chienne", "enculé","pute"]
